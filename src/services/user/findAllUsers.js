95b30b7e69675b8a20f58ce2e393822354fb70ea
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-10-17 19:58:22 -0300

    refactor: service for UserService function findAllUsers exclude attribute password

--- src/services/user/findAllUsers.js
+++ src/services/user/findAllUsers.js
@@ -1,7 +1,9 @@
 const { User } = require('../../models');
 
 module.exports = async () => {
-  const users = await User.findAll();
+  const users = await User.findAll({
+    attributes: { exclude: ['password'] },
+  });
 
   return users;
 };
